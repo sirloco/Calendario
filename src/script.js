201ad24bb2d5880222b142ba9c0b5f7167bdf438
santi <sirlocus66@gmail.com>
2024-04-27 19:20:37 +0200

    Mejora la estetica quitando el desplegable y aumentando la letra

--- src/script.js
+++ src/script.js
@@ -4,16 +4,7 @@ async function createCalendar ({locale, year, zona}) {
     const intlWeekDay = new Intl.DateTimeFormat(locale, {weekday: 'short'})
 
     const festivos = await cargarFestivos();
-    
-/*    festivos.forEach(festividad => {
-        console.log(new Date(festividad.festivity_date).getMonth());
-        console.log("Fecha de Festividad:", festividad.festivity_date);
-        console.log("Nombre de Festividad (Español):", festividad.festivity_name_es);
-        console.log("Ubicación (Español):", festividad.location_es);
-        console.log("--------------------");
-    });*/
-
-    
+        
     // el uno de enero de 2024 cae en lunes al recorrer el mapa genera los dias
     const weekDaysNames = weekDays.map(weekdayIndex => {
         const weekDayName = intlWeekDay.format(new Date(2024, 0, weekdayIndex + 1))
@@ -98,115 +89,46 @@ async function createCalendar ({locale, year, zona}) {
 }
 
 const selectYear = ({year}) => {
-    // Obtener el elemento select
-    var yearSelect = document.getElementById("year-select");
-    
-    const minYear = 1900;
-    const maxYear = 2100;
-    
-    for (var i = minYear; i <= maxYear; i++) {
-        var option = document.createElement("option");
-        option.text = i;
-        option.value = i;
-        yearSelect.add(option);
-      }
-    
-    // Establecer el año actual como la opción predeterminada seleccionada
-    yearSelect.value = year;
-
-    
-}
-// Función para llamar a createCalendar() con el año seleccionado
-function actualizarCalendario() {
-    // Obtener el valor del año seleccionado
-    const yearSeleccionado = parseInt(document.getElementById("year-select").value);
-    
-    // Llamar a createCalendar() con el año seleccionado
-    createCalendar({ year: yearSeleccionado, locale: 'es', zona: "" });
+    // Obtener el elemento span (botonera)
+    var urteSelect = document.getElementById("current-year");
+    // Establecel el año actual como predeterminado entre los botones
+    urteSelect.textContent = year;
 }
 
-// Agregar un listener de eventos para detectar cambios en el menú desplegable
-document.getElementById("year-select").addEventListener("change", actualizarCalendario);
+
+// Función para avanzar un año
+function avanzarAnio() {
+    // Obtener el elemento del año actual
+    const currentYearElement = document.getElementById("current-year");
+    // Obtener el año actual
+    let currentYear = parseInt(currentYearElement.textContent);
+    // Incrementar el año
+    currentYear++;
+    // Actualizar el año en el elemento
+    currentYearElement.textContent = currentYear;
+    // Llamar a createCalendar() con el nuevo año
+    createCalendar({ year: currentYear, locale: 'es', zona: "" });
+}
+
+// Función para retroceder un año
+function retrocederAnio() {
+    // Obtener el elemento del año actual
+    const currentYearElement = document.getElementById("current-year");
+    // Obtener el año actual
+    let currentYear = parseInt(currentYearElement.textContent);
+    // Decrementar el año
+    currentYear--;
+    // Actualizar el año en el elemento
+    currentYearElement.textContent = currentYear;
+    // Llamar a createCalendar() con el nuevo año
+    createCalendar({ year: currentYear, locale: 'es', zona: "" });
+}
+
+// Agregar eventos de clic a los botones de navegación de año
+document.getElementById("prev-year").addEventListener("click", retrocederAnio);
+document.getElementById("next-year").addEventListener("click", avanzarAnio);
 
 let date = new Date(),
 currYear = date.getFullYear()
 selectYear({year:currYear})
 createCalendar({year: currYear, locale: 'es', zona:""})
-
-
-   // var yearSelected = document.getElementById("year-select").value;
-   // console.log("año" + yearSelected)
-   // createCalendar({year: yearSelected, locale: 'es'})
-
-
-
-/////////////////////////////////////////////
-
-/*const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span");
-
-// getting new date, current year and month
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
-
-// se guardan los meses en un array
-const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
-              "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-
-const renderCalendar = () => {
-    // se guarda el primer dia del mes
-    let firstDayofMonth = new Date(currYear, currMonth, 0).getDay(),
-    // se guarda la ultima fecha del mes
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-    // se guarda el ultimo dia del mes
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
-    // se guarda la fecha del mes anterior
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); 
-    let liTag = "";
-
-    // se crea la li de los dias del mes anterior
-    for (let i = firstDayofMonth; i > 0; i--) { 
-        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
-
-    // se crea la li de todos los dias del mes actual
-    for (let i = 1; i <= lastDateofMonth; i++) { 
-        // se añade una class al li del dia actual si coincide con el dia el mes y el año
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                     && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${isToday}">${i}</li>`;
-    }
-
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-
-    }
-
-    // se pasa el texto del mes y el año actual al html
-    currentDate.innerText = `${months[currMonth]}`; 
-    // se pasan todos los li  de los dias al html
-    daysTag.innerHTML = liTag;
-}
-renderCalendar();
-
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
-        renderCalendar(); // calling renderCalendar function
-    });
-});*/
-
-console.log("bruja escarlata");
-console.log("Gambito");
