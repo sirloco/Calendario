93be5137e5c14dbbbea1bc08ce8768cacc609131
santi <sirlocus66@gmail.com>
2025-03-30 14:35:16 +0200

    intentando ver si esta bien configurado

--- middleware.js
+++ middleware.js
@@ -1,19 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-    const auth = req.headers.get('authorization');
-
-    // Credenciales en formato "usuario:contraseña" (Base64)
-    const validUser = "santi";
-    const validPass = "lidt";
-    const validCreds = "Basic " + btoa(`${validUser}:${validPass}`);
-
-    if (auth === validCreds) {
-        return NextResponse.next(); // Permite el acceso
-    }
-
-    return new NextResponse("Acceso Denegado", {
-        status: 401,
-        headers: { "WWW-Authenticate": 'Basic realm="Acceso Restringido"' },
-    });
+    return new NextResponse("Middleware está funcionando", { status: 200 });
 }
+
